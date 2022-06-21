64673ea7a2cfdfbf169d15537aa81ac75fedb53b
eunjuna <106298365+eunjuna@users.noreply.github.com>
2022-06-21 10:14:28 +0900

    test3

--- slide/slide.js
+++ slide/slide.js
@@ -1,16 +1,22 @@
 $(function(){
 
     $(".slide").slick({
-      arrows: false, /* 좌우 버튼 없애려면 false*/ 
-      dots: true, /* 하단 인디게이터 살리려면 true*/
+      arrows: false,
+      dots: true,
       slidesToShow: 1,
       slidesToScroll: 1
     });
-
-    $(".slide2").slick({
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 3
-    });
-
   });
+
+
+
+
+// $(function(){
+
+//     $(".slide2").slick({
+//       arrows: true,
+//       dots: false,
+//       slidesToShow: 3,
+//       slidesToScroll: 3
+//     });
+// });
