38ad16662538323caf537fc0b72480c9eb77c893
eunjuna <106298365+eunjuna@users.noreply.github.com>
2022-06-21 10:21:48 +0900

    test5

---
+++ slide/slide.js
@@ -0,0 +1,22 @@
+$(function(){
+
+    $(".slide").slick({
+      arrows: false,
+      dots: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+  });
+
+
+
+
+// $(function(){
+
+//     $(".slide2").slick({
+//       arrows: true,
+//       dots: false,
+//       slidesToShow: 3,
+//       slidesToScroll: 3
+//     });
+// });
