7464b5c772a020ee4fb04744c934acdd4e550444
eunjuna <106298365+eunjuna@users.noreply.github.com>
2022-06-27 09:44:21 +0900

    test6

--- slide/slide.js
+++ slide/slide.js
@@ -6,17 +6,12 @@ $(function(){
       slidesToShow: 1,
       slidesToScroll: 1
     });
+
+    // $(".slide2").slick({
+    //   infinite: true,
+    //   slidesToShow: 3,
+    //   slidesToScroll: 3
+    // });
+
   });
 
-
-
-
-// $(function(){
-
-//     $(".slide2").slick({
-//       arrows: true,
-//       dots: false,
-//       slidesToShow: 3,
-//       slidesToScroll: 3
-//     });
-// });
